485ddd5284500d2ad82ee68b3c30771cec83a34a
Nate Croteau <natekcroteau@gmail.com>
2020-11-20 23:52:10 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,7 @@
 def greet_characters(array)
-  # Use `each` to enumerate over the provided array
-  #
-  # Print a custom greeting for each element
+
+  array.each do |greet|
+    
 end
 
 def list_dwarves(array)
