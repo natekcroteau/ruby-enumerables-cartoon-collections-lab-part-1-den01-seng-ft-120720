f53b227b82d9d9fecf1d73329b4a62e0f8dd6659
Nate Croteau <natekcroteau@gmail.com>
2020-11-20 23:56:48 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,11 @@ def greet_characters(array)
 end
 
 def list_dwarves(array)
-  # Use `each_with_index` to enumerate over the provided array
+  array.each_with_index do |name|
+    puts #{name}
+  end 
+end
+
+# Use `each_with_index` to enumerate over the provided array
   #
   # Print a numbered list of each element
-end
