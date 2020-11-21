9077deefbb8af17a94f441be4a6aaba6125cd348
Nate Croteau <natekcroteau@gmail.com>
2020-11-21 00:18:05 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,7 +8,7 @@ end
 
 def list_dwarves(array)
   array.each_with_index do |name, index|
-    puts "#{index + 1}. #{name}"
+    puts "#{index + 1}. #{name}"             # + 1 is to make it a human readable list starting with 1 from the index 0 
   end 
 end
 
